d34313b522a06a2a523c8a815f900f5268b9cc03
Abzal Shaken <abzalshaken@Abzals-MacBook-Pro-2.local>
2025-01-10 23:12:39 -0600

    terraform new code

--- main.tf
+++ main.tf
@@ -1,14 +1,22 @@
 resource "aws_s3_bucket" "first_bucket" {
-  bucket = "abzal-first-bucket"
-  tags = batch-14
+  bucket = "abzal-bucket-one"
 }
 
 resource "aws_s3_bucket" "second_bucket" {
-  bucket = "abzal-second-bucket"
-  tags = batch-14
+  bucket = "abzal-bucket-two"
 }
 
 resource "aws_s3_bucket" "third_bucket" {
-  bucket = "abzal-third-bucket"
-  tags = batch-14
+  bucket = "abzal-bucket-three"
+}
+resource "aws_vpc" "vpc_one" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "vpc_two" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_vpc" "vpc_three" {
+  cidr_block = "10.2.0.0/16"
 }
